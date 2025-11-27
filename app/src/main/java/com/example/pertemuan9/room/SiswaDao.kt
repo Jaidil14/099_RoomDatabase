7a6446dbeb10b5e1a444648fddd03f328ce7ead2
Jaidil14 <jaidilrachmad@gmail.com>
2025-11-27 13:08:54 +0700

    menambahkan file dan isi SiswaDao.kt

---
+++ app/src/main/java/com/example/pertemuan9/room/SiswaDao.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan9.room
+
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
