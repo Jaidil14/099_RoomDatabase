cd9eea3db64756739b20b2b9a510b4e473746ea8
Jaidil14 <jaidilrachmad@gmail.com>
2025-12-04 12:01:27 +0700

    Menambahkan isi library dan dependensi yg diperlukan

--- app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/DetailViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pertemuan9.viewmodel
 
-
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan9.repositori.RepositoriSiswa
+import com.example.pertemuan9.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
