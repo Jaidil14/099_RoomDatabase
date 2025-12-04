412820ab624df7d648a6720c8a0454a74bf4dedc
Jaidil14 <jaidilrachmad@gmail.com>
2025-12-04 13:18:15 +0700

    Menambahkan initializer untuk viewmodel

--- app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/pertemuan9/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.pertemuan9.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.pertemuan9.repositori.AplikasiSiswa
+import com.example.pertemuan9.viewmodel.DetailViewModel
 import com.example.pertemuan9.viewmodel.EntryViewModel
 import com.example.pertemuan9.viewmodel.HomeViewModel
 
@@ -17,6 +19,12 @@ object PenyediaViewModel {
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
+        // Edit: tambah initializer untuk DetailViewModel dan EditViewModel
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
+
     }
 }
 
